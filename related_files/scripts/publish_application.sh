df1f2558465f27c941e81afa745fc97eae37b324
ghislain.coutu <gc8395@gmail.com>
2022-12-26 13:18:59 -0500

    Exercice Angular

--- related_files/scripts/publish_application.sh
+++ related_files/scripts/publish_application.sh
@@ -6,7 +6,7 @@ echo "Publication de l'applications sur le serveur Web local"
 apply_pause "Appuyer sur la touche [Retour] pour continuer..."
 
 # Source (so)
-so=/home/Exercices/angular-e03/dist/angular-e03
+so=/home/Exercises/angular-e03/dist/angular-e03
 # Destination (de)
 de=/var/www/html/r002/angular-e03
 
